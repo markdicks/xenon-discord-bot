afa71bc187e4a4da651379517d4fd5913d95d376
markdicks <markdicks03@gmail.com>
2024-11-13 17:28:41 +0200

    Re-word of code Phase 1

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const express = require('express');
 const bcrypt = require("bcrypt");
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
+import fetch from 'node-fetch';
 
 const token = process.env.DISCORD_TOKEN;
 
@@ -48,28 +49,63 @@ const client = new Client({
 client.once("ready", async () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setPresence({
-        activities: [
-            { name: "xenonesports.gg", type: ActivityType.Playing },
-        ],
+        activities: [{
+            name: "xenonesports.gg",
+            type: ActivityType.Playing
+        }, ],
         status: "online",
     });
 
-    const rest = new REST({ version: "10" }).setToken(token);
+    const rest = new REST({
+        version: "10"
+    }).setToken(token);
 
     try {
         console.log("Started refreshing application (/) commands.");
 
         await rest.put(Routes.applicationCommands(client.user.id), {
-            body: [
-                { name: "hello", description: "I will greet you ^^" },
-                { name: "about", description: "Sends a DM about Xenon Esports" },
-                { name: "show-rank", description: "Shows your current mmr." },
-                { name: "time", description: "Replies with the current time" },
-                { name: "help", description: "Displays a list of available commands" },
-                { name: "register", description: "Register a new account" },
-                { name: "login", description: "Login to your account" },
-                { name: "change-password", description: "Change your account password" },
-                { name: "create-scrim", description: "Schedule a scrim", options: [{ name: "when", type: 3, description: "When is the scrim?", required: true }] },
+            body: [{
+                    name: "hello",
+                    description: "I will greet you ^^"
+                },
+                {
+                    name: "about",
+                    description: "Sends a DM about Xenon Esports"
+                },
+                {
+                    name: "show-rank",
+                    description: "Shows your current mmr."
+                },
+                {
+                    name: "time",
+                    description: "Replies with the current time"
+                },
+                {
+                    name: "help",
+                    description: "Displays a list of available commands"
+                },
+                {
+                    name: "register",
+                    description: "Register a new account"
+                },
+                {
+                    name: "login",
+                    description: "Login to your account"
+                },
+                {
+                    name: "change-password",
+                    description: "Change your account password"
+                },
+                {
+                    name: "create-scrim",
+                    description: "Schedule a scrim",
+                    options: [{
+                        name: "when",
+                        type: 3,
+                        description: "When is the scrim?",
+                        required: true
+                    }]
+                },
             ],
         });
 
@@ -100,7 +136,11 @@ function initializeDatabase(serverId) {
 // Command handler
 client.on("interactionCreate", async (interaction) => {
     if (interaction.isCommand()) {
-        const { commandName, guildId, user } = interaction;
+        const {
+            commandName,
+            guildId,
+            user
+        } = interaction;
         const db = initializeDatabase(guildId);
 
         // Command handling
@@ -126,7 +166,7 @@ client.on("interactionCreate", async (interaction) => {
             const modal = new ModalBuilder()
                 .setCustomId("change_password_modal")
                 .setTitle("Change Your Password");
-        
+
             const oldPasswordInput = new TextInputBuilder()
                 .setCustomId("old_password_input")
                 .setLabel("Old Password")
@@ -134,7 +174,7 @@ client.on("interactionCreate", async (interaction) => {
                 .setMinLength(3)
                 .setMaxLength(12)
                 .setRequired(true);
-        
+
             const newPasswordInput = new TextInputBuilder()
                 .setCustomId("password_input")
                 .setLabel("New Password")
@@ -142,82 +182,117 @@ client.on("interactionCreate", async (interaction) => {
                 .setMinLength(3)
                 .setMaxLength(12)
                 .setRequired(true);
-        
+
             const actionRowOldPassword = new ActionRowBuilder().addComponents(oldPasswordInput);
             const actionRowNewPassword = new ActionRowBuilder().addComponents(newPasswordInput);
             modal.addComponents(actionRowOldPassword, actionRowNewPassword);
-        
+
             await interaction.showModal(modal);
-        }else if (commandName === "hello") {
-            await interaction.reply({ content: "Hi, Welcome to Xenon Esports! Use the /about command to learn more about us.", ephemeral: true });
+        } else if (commandName === "hello") {
+            await interaction.reply({
+                content: "Hi, Welcome to Xenon Esports! Use the /about command to learn more about us.",
+                ephemeral: true
+            });
         } else if (commandName === "about") {
             await interaction.user.send("Xenon Esports is an org that started to create a community that shows love and respect to each other. Check out our website for more info here: https://xenonesportsgg.com");
-            await interaction.reply({ content: "I have sent you a DM with information about Xenon Esports.", ephemeral: false });
+            await interaction.reply({
+                content: "I have sent you a DM with information about Xenon Esports.",
+                ephemeral: false
+            });
         } else if (commandName === "time") {
             const currentTime = new Date();
             const timeString = `<t:${Math.floor(currentTime.getTime() / 1000)}:F>`; // Full timestamp format
-            await interaction.reply({ content: `The current time is ${timeString}`, ephemeral: true });
+            await interaction.reply({
+                content: `The current time is ${timeString}`,
+                ephemeral: true
+            });
         } else if (commandName === "help") {
             const embed = new EmbedBuilder()
                 .setTitle("Xenon Esports Bot Commands")
                 .setDescription("Here are the available commands:")
-                .addFields(
-                    { name: "/hello", value: "Replies with a greeting message." },
-                    { name: "/about", value: "Sends a DM with information about Xenon Esports." },
-                    { name: "/time", value: "Replies with the current time." },
-                    { name: "/create-scrim", value: "Schedule a scrim by providing the time." },
-                    { name: "/help", value: "Displays this help message." },
-                    { name: "/change-password", value: "Allows you to change the password for login." },
-                )
-                .setFooter({ text: "Use the commands by typing / followed by the command name." });
+                .addFields({
+                    name: "/hello",
+                    value: "Replies with a greeting message."
+                }, {
+                    name: "/about",
+                    value: "Sends a DM with information about Xenon Esports."
+                }, {
+                    name: "/time",
+                    value: "Replies with the current time."
+                }, {
+                    name: "/create-scrim",
+                    value: "Schedule a scrim by providing the time."
+                }, {
+                    name: "/help",
+                    value: "Displays this help message."
+                }, {
+                    name: "/change-password",
+                    value: "Allows you to change the password for login."
+                }, )
+                .setFooter({
+                    text: "Use the commands by typing / followed by the command name."
+                });
 
-            await interaction.reply({ embeds: [embed], ephemeral: false });
+            await interaction.reply({
+                embeds: [embed],
+                ephemeral: false
+            });
         } else if (commandName === "create-scrim") {
             const when = interaction.options.getString("when");
-            await interaction.reply({ content: `Scrim scheduled for ${when}`, ephemeral: false });
-        }else if (commandName === "show-rank") {
-            const apiUrl = 'https://api.tracker.gg/api/v2/rocket-league/standard/profile/epic/sparkycracked';
-            const apiKey = process.env.TRN_API_KEY;
+            await interaction.reply({
+                content: `Scrim scheduled for ${when}`,
+                ephemeral: false
+            });
+        } else if (commandName === "show-rank") {
+            const pageUrl = 'https://tracker.gg/rocket-league/profile/epic/sparkycracked/overview';
         
             try {
-                const response = await fetch(apiUrl, {
-                    method: 'GET',
-                    headers: {
-                        'TRN-Api-Key': apiKey,
-                        'User-Agent': 'XenonDiscordBot/1.0',
-                    }
-                });
+                // Fetch the raw HTML text from the page
+                const response = await fetch(pageUrl);
         
-                console.log(`API Response Status: ${response.status}`);
-        
+                // Check if the response is okay
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}, Message: ${response.statusText}`);
                 }
         
-                const data = await response.json();
-                console.log('API Data:', data);
+                const textData = await response.text();
         
-                const ranked2v2Segment = data.data.segments.find(segment => segment.attributes.playlist === 'Ranked Doubles 2v2');
+                // Log the raw HTML (for debugging purposes, you can remove this once confirmed)
+                console.log(textData);
         
-                if (ranked2v2Segment) {
-                    const mmr = ranked2v2Segment.attributes.mmr;
+                // Search for the rank information in the page text
+                const rankMatch = textData.match('/Ranked Doubles 2v2.*?rating:\s*(\d+)/i');
+        
+                if (rankMatch && rankMatch[1]) {
+                    const mmr = rankMatch[1];  // Extract the MMR
                     await interaction.reply({ content: `Your current rank: ${mmr}`, ephemeral: false });
                 } else {
                     await interaction.reply({ content: "Ranked Doubles 2v2 data not found.", ephemeral: true });
                 }
             } catch (error) {
-                console.error('Error fetching data:', error);
-                await interaction.reply({ content: 'Failed to retrieve rank data. Please try again later or check the user profile.', ephemeral: true });
+                // Log the error for debugging purposes
+                console.error('Error processing data:', error);
+        
+                // Inform the user of failure
+                await interaction.reply({
+                    content: 'Failed to retrieve or process rank data. Please try again later or contact an admin.',
+                    ephemeral: true
+                });
             }
         }
         
+        
+
     } else if (interaction.isModalSubmit()) {
-        const { guildId, user } = interaction;
+        const {
+            guildId,
+            user
+        } = interaction;
         const db = initializeDatabase(guildId);
         const command = interaction.customId.split("_")[0];
         const password = interaction.fields.getTextInputValue("password_input");
         const newPassword = password;
-        
+
 
         if (command === "register") {
             try {
@@ -225,32 +300,56 @@ client.on("interactionCreate", async (interaction) => {
                 db.run("INSERT INTO users (userId, hashedPassword) VALUES (?, ?)", [user.id, hashedPassword], (err) => {
                     if (err) {
                         if (err.code === "SQLITE_CONSTRAINT") {
-                            interaction.reply({ content: "You already have an account.", ephemeral: true });
+                            interaction.reply({
+                                content: "You already have an account.",
+                                ephemeral: true
+                            });
                         } else {
                             console.error("Database error during registration:", err);
-                            interaction.reply({ content: "Error creating account.", ephemeral: true });
+                            interaction.reply({
+                                content: "Error creating account.",
+                                ephemeral: true
+                            });
                         }
                     } else {
-                        interaction.reply({ content: "Account registered successfully!", ephemeral: false });
+                        interaction.reply({
+                            content: "Account registered successfully!",
+                            ephemeral: false
+                        });
                     }
                 });
             } catch (error) {
                 console.error("Error hashing password:", error);
-                interaction.reply({ content: "An error occurred during registration.", ephemeral: true });
+                interaction.reply({
+                    content: "An error occurred during registration.",
+                    ephemeral: true
+                });
             }
         } else if (command === "login") {
             db.get("SELECT hashedPassword FROM users WHERE userId = ?", [user.id], async (err, row) => {
                 if (err) {
                     console.error("Database error during login:", err);
-                    interaction.reply({ content: "Error logging in.", ephemeral: true });
+                    interaction.reply({
+                        content: "Error logging in.",
+                        ephemeral: true
+                    });
                 } else if (!row) {
-                    interaction.reply({ content: "No account found. Please register first.", ephemeral: true });
+                    interaction.reply({
+                        content: "No account found. Please register first.",
+                        ephemeral: true
+                    });
                 } else {
                     const isMatch = await bcrypt.compare(password, row.hashedPassword);
                     if (isMatch) {
-                        interaction.reply({ content: "Login successful!", ephemeral: false });
+                        interaction.reply({
+                            content: "Login successful!",
+                            ephemeral: false
+                        });
                     } else {
-                        interaction.reply({ content: "Incorrect password.", ephemeral: false });
+                        interaction.reply({
+                            content: "Incorrect password.",
+                            ephemeral: false
+                        });
                     }
                 }
             });
@@ -259,36 +358,54 @@ client.on("interactionCreate", async (interaction) => {
             db.get("SELECT hashedPassword FROM users WHERE userId = ?", [user.id], async (err, row) => {
                 if (err) {
                     console.error("Database error during password change:", err);
-                    interaction.reply({ content: "Error accessing the database.", ephemeral: true });
+                    interaction.reply({
+                        content: "Error accessing the database.",
+                        ephemeral: true
+                    });
                     return;
                 }
-    
+
                 if (!row) {
-                    interaction.reply({ content: "No account found. Please register first.", ephemeral: true });
+                    interaction.reply({
+                        content: "No account found. Please register first.",
+                        ephemeral: true
+                    });
                     return;
                 }
-    
+
                 // Compare old password
                 const isOldPasswordMatch = await bcrypt.compare(oldPassword, row.hashedPassword);
                 if (!isOldPasswordMatch) {
-                    interaction.reply({ content: "Old password is incorrect.", ephemeral: true });
+                    interaction.reply({
+                        content: "Old password is incorrect.",
+                        ephemeral: true
+                    });
                     return;
                 }
-    
+
                 // Hash new password and update
                 try {
                     const hashedNewPassword = await bcrypt.hash(newPassword, 10);
                     db.run("UPDATE users SET hashedPassword = ? WHERE userId = ?", [hashedNewPassword, user.id], (err) => {
                         if (err) {
                             console.error("Error updating password:", err);
-                            interaction.reply({ content: "Error updating password. Please try again.", ephemeral: true });
+                            interaction.reply({
+                                content: "Error updating password. Please try again.",
+                                ephemeral: true
+                            });
                         } else {
-                            interaction.reply({ content: "Password changed successfully!", ephemeral: false });
+                            interaction.reply({
+                                content: "Password changed successfully!",
+                                ephemeral: false
+                            });
                         }
                     });
                 } catch (error) {
                     console.error("Error hashing new password:", error);
-                    interaction.reply({ content: "Error processing the new password.", ephemeral: true });
+                    interaction.reply({
+                        content: "Error processing the new password.",
+                        ephemeral: true
+                    });
                 }
             });
         }
